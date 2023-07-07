27134fe58a40796e1f4f3b072d36d910cbc59274
Gianpaolo Papaiz <gianpaolopapaiz@hotmail.com>
2023-07-06 23:58:19 -0300

    Replace components by material ui ones

--- src/Components/Pages/AccountsPage.jsx
+++ src/Components/Pages/AccountsPage.jsx
@@ -1,8 +1,23 @@
+import { Breadcrumbs, Link } from "@mui/material";
 import WalletTable from "../WalletTable/WalletTable";
+import Typography from '@mui/material/Typography';
 
 const AccountsPage = () => {
   return (
     <div id="accounts-page">
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link underline="hover" color="inherit" href="/">
+          MUI
+        </Link>
+        <Link
+          underline="hover"
+          color="inherit"
+          href="/material-ui/getting-started/installation/"
+        >
+          Core
+        </Link>
+        <Typography color="text.primary">Breadcrumbs</Typography>
+      </Breadcrumbs>
       <h3> Accounts Overview </h3>
       <div className="page-card">
         <WalletTable />
